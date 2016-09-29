b85895f8f0895c488fa60795a951c4ecf9c55adc
Hsing-Li <chang@hsing.li>
2016-09-29 15:21:55 -0600

    allow origin

--- public/index.php
+++ public/index.php
@@ -47,6 +47,11 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Authorization,Origin, X-Requested-With, Content-Type, Accept");
+
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
