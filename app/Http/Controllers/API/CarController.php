a257393b4abda191020f4e0947a25274f24692d3
hlca <hsinglic@gmail.com>
2016-11-15 14:09:43 -0600

    Update CarController.php
    Car creation

--- app/Http/Controllers/API/CarController.php
+++ app/Http/Controllers/API/CarController.php
@@ -15,4 +15,17 @@ class CarController extends Controller
   	$car = Car::with('color')->with('car_brand')->with('user')->where('id', $request->car_id)->first();
   	return response()->json(['data' => $car]);
   }
+  
+  public function newCar(Request $request){
+    $car = new Car;
+    $car->color_id = 1;
+    $car->car_brand_id = 1;
+    $car->car_model = $request->model;
+    $car->line = $request->line;
+    $car->available_spots = 10;
+    $car->user_id = $request->user_id;
+    $car->save();
+    return respnse()->json(['data' => $car, 'success' => true]);
+  }
+  
 }
